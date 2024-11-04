e10b9e9cdb2ac03c5ea1db31c3e212d462eec01f
Akshayan-N <snakshayan2@gmail.com>
2024-11-04 06:27:05 +0000

    updates

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template, make_response, redirect, url_for, flash, send_file, jsonify, session
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from datetime import timedelta
-import io
+from io import BytesIO
 import json
 import base64
 import os
@@ -15,38 +15,39 @@ app.secret_key = "mini-inventory"
 
 # Initialize Flask-Login
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'  # Where to redirect if user is not logged in
-login_manager.login_message_category = 'warning'  # Flash message category for login required
+login_manager.login_view = 'login' 
+login_manager.login_message_category = 'warning'  
 
-# Load environment variables
+# Load env values
 load_dotenv()
 DB_HOST = os.getenv('DB_HOST')
 DB_USER = os.getenv('DB_USER')
 DB_PASSWD = os.getenv('DB_PASSWD')
 
-# Database connection
-connection = mysql.connector.connect(
-    host=DB_HOST, 
-    user=DB_USER,
-    password=DB_PASSWD, 
-    database="mini",
-    port=24967
-)
+# DB connection
+dbconfig = {
+    "host": DB_HOST, 
+    "user": DB_USER,
+    "password":DB_PASSWD, 
+    "database":"mini",
+    "port": 24967,
+}
 
+connection_pool = mysql.connector.pooling.MySQLConnectionPool(
+    pool_name="mypool",
+    pool_size=32,  # Adjust the pool size as needed
+    **dbconfig
+)
 class User(UserMixin):
     """Define a simple User model. Flask-Login requires this."""
     def __init__(self, id, username, password):
         self.id = id
         self.username = username
         self.password = password
-# Load user from the database by their ID
+# Load user from the db by their ID
 @login_manager.user_loader
 def load_user(user_id):
-    cursor = connection.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM users WHERE userID = %s", (user_id,))
-    user = cursor.fetchone()
-    cursor.close()
-
+    user = sql_execute("SELECT * FROM users WHERE userID = %s", (user_id,))[0]
     if user:
         return User(id=user['userID'], username=user['username'], password=user['password'])
     return None
@@ -57,95 +58,39 @@ def load_user(user_id):
 def index():
     return render_template("index.html")
 
-@app.route('/verify_password', methods=['POST'])
-def verify_password():
-    data = request.json
-    password = data.get('password')
-
-    # Assume you have a database connection 'conn'
-    conn = connection  
-    cursor = conn.cursor(dictionary=True)
-    
-    # Retrieve the hashed password for the admin user
-    cursor.execute("SELECT password FROM users WHERE username = 'admin';")
-    result = cursor.fetchone()  # Fetch the first matching result
-    cursor.close()
-
-    # Check if the admin user exists and if the provided password matches the stored hash
-    if result and check_password_hash(result['password'], password):
-        return jsonify(success=True), 200
-    else:
-        return jsonify(success=False), 403
-
-@app.route('/materials', methods=['GET', 'POST'])
+@app.route('/materials', methods=['GET'])
 def materials():
-    conn = connection  
-    cursor = conn.cursor(dictionary=True)
-    
-    if request.method == 'POST':
-        action = request.form['action']
-        name = request.form['name']
-        quantity = request.form.get('quantity', 0)
-
-        if action == 'add':
-            cursor.execute("INSERT INTO materials (name, quantity, remaining, used) VALUES (%s, %s, %s, %s)", 
-                           (name, quantity, quantity, 0))
-            flash("Material added successfully!")
-        elif action == 'update':
-            cursor.execute("UPDATE materials SET quantity = %s, remaining = remaining + %s WHERE name = %s", 
-                           (quantity, quantity, name))
-            flash("Material updated successfully!")
-        elif action == 'remove':
-            cursor.execute("DELETE FROM materials WHERE name = %s", (name,))
-            flash("Material removed successfully!")
-        
-        conn.commit()
-
-    cursor.execute("SELECT * FROM materials")
-    materials = cursor.fetchall()
-
-    
-    cursor.close()
-
-    return render_template('materials.html', materials=materials,admin_password="Hello World")
+    materials = sql_execute("SELECT * FROM materials")
+    return render_template('materials.html', materials=materials)
 
 @app.route('/material/add', methods=['GET', 'POST'])
 @login_required
 def add_material():
     if request.method == 'POST':
-        name = request.form['name']
-        quantity = request.form['quantity']
-        material_type = request.form['type']
-
-        cursor = connection.cursor()
-        cursor.execute("INSERT INTO materials (name, quantity, type) VALUES (%s, %s, %s)", (name, quantity, material_type))
-        connection.commit()
+        name,quantity,material_type = request.form['name'], request.form['quantity'], request.form['type']
+        sql_execute("INSERT INTO materials (name, quantity, type) VALUES (%s, %s, %s)", (name, quantity, material_type))
         flash("Material added successfully!")
+        
         return redirect(url_for('materials'))
-
     return render_template('material/add.html')
 
 @app.route('/material/update', methods=['GET', 'POST'])
 @login_required 
-def update_material():
-    cursor = connection.cursor(dictionary=True)
-    
+def update_material():    
     if request.method == 'POST':
         material_id = request.form['id']
         new_name = request.form['name']
         new_quantity = request.form['quantity']
         new_type = request.form['type']
 
-        cursor.execute("UPDATE materials SET name = %s, quantity = %s, type = %s WHERE id = %s", 
+        sql_execute("UPDATE materials SET name = %s, quantity = %s, type = %s WHERE id = %s", 
                        (new_name, new_quantity, new_type, material_id))
-        connection.commit()
+
         flash("Material updated successfully!")
         return redirect(url_for('materials'))
 
-    cursor.execute("SELECT * FROM materials")
-    materials = cursor.fetchall()    
+    materials = sql_execute("SELECT * FROM materials")  
     return render_template('material/update.html', materials=materials)
-
 
 @app.route('/material/remove', methods=['GET', 'POST'])
 @login_required
@@ -155,57 +100,74 @@ def remove_material():
     if request.method == 'POST':
         material_id = request.form['id']
 
-        cursor.execute("DELETE FROM materials WHERE id = %s", (material_id,))
-        connection.commit()
-
-        cursor.execute("SET @new_id = 0;")
-        cursor.execute("UPDATE materials SET id = (@new_id := @new_id + 1);")
-        connection.commit()
-
-        cursor.execute("ALTER TABLE materials AUTO_INCREMENT = 1;")
-        connection.commit()
+        sql_execute("DELETE FROM materials WHERE id = %s", (material_id,))
+        sql_execute("SET @new_id = 0;")
+        sql_execute("UPDATE materials SET id = (@new_id := @new_id + 1);")
+        sql_execute("ALTER TABLE materials AUTO_INCREMENT = 1;")
 
         flash("Material removed and IDs reordered successfully!")
         return redirect(url_for('materials'))
 
-    cursor.execute("SELECT id, name FROM materials")
-    materials = cursor.fetchall()
+    materials =sql_execute("SELECT id, name FROM materials")
     return render_template('material/remove.html', materials=materials)
 
 @app.route("/products", methods=["GET", "POST"])
-def available_products():
-    cursor = connection.cursor(dictionary=True)
-    
-    cursor.execute("SELECT * FROM products;") 
-    products = cursor.fetchall()
-
+def products():
+    products = sql_execute("SELECT PID, name, Price, Descript, Img FROM products WHERE InStockCount > 0;")
     for product in products:
-        product["Img"] = base64.b64encode(product["Img"]).decode('utf-8')
-    
-    cursor.close()
+        if product['Img'] is not None:
+            product['Img'] = base64.b64encode(product['Img']).decode('utf-8')
     return render_template("products.html", products=products)
 
-@app.route("/product", methods=["GET", "POST"])
-def product():
-    if request.method == "POST":
-        new_product = request.form.to_dict()
-        image = request.files['image'].read()
+@app.route("/product/<int:id>", methods=["GET"])
+def product(id):
+    product = sql_execute("SELECT PID, name, price, Img, Descript, category FROM products WHERE PID = %s AND InStockCount > 0", (id, ))
 
-        cursor = connection.cursor()
-        cursor.execute("INSERT INTO products (name, category, psize, Price, InStockCount, Img, Descript) \
-                        VALUES (%s, %s, %s, %s, %s, %s, %s)", 
-                       (new_product["name"], new_product["category"], new_product["psize"], 
-                        new_product["price"], new_product["in_stock_count"], image, new_product["description"]))
-        connection.commit()
-        cursor.close()
+    if not product:
+        return f"Product ID - {id} not found in the database", 404
 
-        return redirect("/products")
+    product = product[0]  # Get the first result
+    
+    if product['Img'] is not None:
+        # Convert the binary image to base64
+        product['Img'] = "data:image/jpeg;base64," + base64.b64encode(product['Img']).decode('utf-8')
 
+    return render_template("product.html", product=product)
 
-@app.route("/product/add")
+@app.route("/product/add", methods=['GET', 'POST'])
 @login_required 
 def add_product():
-    return render_template("add_product.html")
+    if request.method == 'POST':
+        name = request.form['name']
+        category = request.form['category']
+        psize = request.form['psize']
+        price = request.form['price']
+        in_stock_count = request.form['in_stock_count']
+        description = request.form['description']
+        image = request.files['image'].read()
+        sql_execute("INSERT INTO products (name, category, psize, Price, InStockCount, Descript, Img) VALUES (%s, %s, %s, %s, %s, %s, %s)", (name, category, psize, price, in_stock_count, description, image))
+        flash('Product added successfully!', 'success')
+        return redirect(url_for('products'))
+    return render_template("product/add.html")
+
+@app.route("/product/remove" , methods=['GET', 'POST'])
+@login_required 
+def remove_product():
+    if request.method == "POST" :
+        id = request.form["product_id"]
+        sql_execute("DELETE FROM products WHERE PID = %s", (id,))
+        sql_execute("SET @new_id = 0;")
+        sql_execute("UPDATE products SET id = (@new_id := @new_id + 1);")
+        sql_execute("ALTER TABLE products AUTO_INCREMENT = 1;")        
+        flash('Product removed successfully!', 'success')
+        return redirect("/products")
+    products = sql_execute("select PID, name, category from products;")
+    return render_template("product/remove.html", products=products)
+
+@app.route("/product/update")
+@login_required 
+def update_product():
+    return render_template("product/update.html")
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
@@ -213,9 +175,7 @@ def login():
         username = request.form.get('username').strip()
         password = request.form.get('password').strip()
 
-        cursor = connection.cursor(dictionary=True)
-        cursor.execute("select userID, password from users where username=%s" ,(username, ))
-        user = cursor.fetchone()
+        user = sql_execute("select userID, password from users where username=%s" ,(username, ))[0]
         if user and check_password_hash(user["password"], password):
             logged_in_user = User(id=user['userID'], username=username, password=user['password'])
             login_user(logged_in_user)
@@ -225,6 +185,33 @@ def login():
 
     return render_template("login.html")
 
+@app.route('/transactions')
+@login_required
+def transactions():
+    # cursor = connection.cursor(dictionary=True)
+    # cursor.execute("SELECT * FROM transactions;") 
+    # transactions = cursor.fetchall()
+
+    return render_template("payment.html")
+
+@app.route('/transaction/add')
+@login_required
+def add_transactions():
+    return render_template("add_transaction.html")
+
+@app.route('/transaction/bill')
+@login_required
+def bill_transactions():
+    return render_template("bill.html")
+
+@app.route('/customize')
+def customize_status():
+    return render_template("customize_status.html")
+
+@app.route('/customize/<int:id>')
+def customize(id):
+    product = sql_execute("select PID, name from products where PID = %s", (id, ))[0]
+    return render_template("customize.html", product=product)
 
 @app.route('/logout')
 def logout():
@@ -238,7 +225,30 @@ def not_found(error=None):
 @app.before_request
 def before_request():
     session.permanent = True
-    app.permanent_session_lifetime = timedelta(seconds=10)
+    app.permanent_session_lifetime = timedelta(minutes=30)
+
+def sql_execute(command, fields=()):
+    connection = connection_pool.get_connection()
+    cursor = connection.cursor(dictionary=True, buffered=True)
+    try:
+        cursor.execute(command, fields)
+        output = cursor.fetchall()  # Fetch the result
+    except mysql.connector.errors.InternalError as e:
+        # Handle the unread result error by clearing old results
+        if "Unread result found" in str(e):
+            cursor.fetchall()  # Clear the unread results
+            cursor.execute(command, fields)  # Re-run the query
+            output = cursor.fetchall()
+        else:
+            output = []
+    except:
+        output = []
+    finally:
+        connection.commit()  
+        cursor.close()
+        connection.close()
+    return output
+
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", debug=True)
